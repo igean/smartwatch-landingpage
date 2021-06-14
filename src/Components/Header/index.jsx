d3c196c3c99080f54b1066a009371260fb24e0e9
Gean Araujo <67212833+igean@users.noreply.github.com>
2021-06-14 11:29:30 -0300

    changes

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,9 +1,10 @@
 import styles from "../../styles/Header.module.css";
-
+import { BiMenuAltRight } from "react-icons/bi";
 export default function Header() {
   return (
     <header className={styles.Header}>
       <h1 className={styles.Logo}>SmartWatch</h1>
+      <BiMenuAltRight id={styles.MenuIcon} />
       <div className={styles.Menu}>
         <a href="/">Home</a>
         <a href="/">Home</a>
