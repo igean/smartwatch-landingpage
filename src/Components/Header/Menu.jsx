c274706af915d28161960472321dd80ab0b830c3
Gean Araujo <67212833+igean@users.noreply.github.com>
2021-06-14 21:30:01 -0300

    menu toggle

--- src/Components/Header/Menu.jsx
+++ src/Components/Header/Menu.jsx
@@ -2,14 +2,32 @@ import { BiMenuAltRight } from "react-icons/bi";
 import { IconContext } from "react-icons";
 
 import styles from "../../styles/Menu.module.css";
+import { useState } from "react";
 
 export function Menu() {
+  const open_value = "-200%";
+
+  const [menu_state, setMenuState] = useState(open_value);
+
+  const handleMenuClick = () => {
+    if (menu_state === "-200%") {
+      setMenuState("0");
+    } else {
+      setMenuState("-200%");
+    }
+  };
+
   return (
     <div>
       <IconContext.Provider value={{ size: "1.8rem" }}>
-        <BiMenuAltRight />
+        <BiMenuAltRight onClick={handleMenuClick} />
       </IconContext.Provider>
-      <div className={styles.Menu}>
+      <div
+        className={styles.Menu}
+        style={{
+          transform: `translateY(${menu_state})`
+        }}
+      >
         <a href="/">Home</a>
         <a href="/">Home</a>
         <a href="#features">Features</a>
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -2,9 +2,11 @@ import styles from "../../styles/Header.module.css";
 import { Menu } from "./Menu";
 export default function Header() {
   return (
-    <header className={styles.Header}>
-      <h1 className={styles.Logo}>SmartWatch</h1>
-      <Menu />
-    </header>
+    <>
+      <header className={styles.Header}>
+        <h1 className={styles.Logo}>SmartWatch</h1>
+        <Menu />
+      </header>
+    </>
   );
 }
